e4f45a8ae44fb161af4d8e832d64963e0bd08355
Qrozyer <10huruf@gmail.com>
2022-12-28 10:10:51 +0700

    day 1

---
+++ day1.php
@@ -0,0 +1,56 @@
+<?php
+
+// echo "program ganjil_genap" .PHP_EOL;
+// // menentukan bilangan ghanjil atau ghenap
+
+// $number = 17;
+
+// if($number % 2 == 0){
+//     echo "genap" . PHP_EOL;
+// }else{
+//     echo "ganjil" . PHP_EOL;
+// }
+
+// /* 
+// PROGRAM HITUNGLUASSEGITIGA
+// DEKLARASI
+// VARIABEL a dan t tipe data >>> int
+// VARIABEL hasil tipe data >>> float
+// Algoritma
+// nilai masuk(a,t)
+// hasil = 1/2 * a * t
+// nilai keluar ('luas segitiga: hasil')
+// */
+
+// // a = 6
+// // t = 20
+
+// echo PHP_EOL;
+// echo "program : menghitung luas segitiga";
+// echo PHP_EOL;
+// $a = 6;
+// $t = 20;
+// $hasil =(string)(1/2 * $a * $t);
+
+// // catatan
+// // output : echo(semua tipe data), print(keluar string)
+// // debugging : var_dump, print_r
+// print("Luas Segitiga:".$hasil). PHP_EOL;
+// echo "Luas Segitiga: ", $a ,"x", $t ,"=", $hasil . PHP_EOL;
+// var_dump($hasil);
+
+
+/* 
+program hitung volume balok
+deklarasi 
+var p, l, t int
+var hasil string
+algoritma
+input(p,l,t)
+hasil = p x l x t
+output 
+Volume Balok : 
+82 x 46 x 18 = hasil
+*/
+echo 'Volume Balok :'. PHP_EOL .'82 x 46 x 18 = $hasil' .PHP_EOL;
+
